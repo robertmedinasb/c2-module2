1b707b688ff0a540901235c95cf37b098a6e341e
robert-medina <robertmedib1@gmail.com>
2020-04-19 23:41:26 -0500

    Refactored

---
+++ week-1/day-1/bmi_input.rb
@@ -0,0 +1,8 @@
+print 'Show me the weight(kg): ' # get mass
+mass = gets.chomp.to_f
+
+print 'Show me the height(m): '  # get height
+height = gets.chomp.to_f
+
+bmi = (mass / height**2).round(2) # calculate
+puts bmi
