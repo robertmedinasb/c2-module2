17ad823476951831ea2e36bbaf247a65101f3e9f
robert-medina <robertmedib1@gmail.com>
2020-04-20 00:19:36 -0500

    Refactored

--- week-1/day-2/count-words.rb
+++
@@ -1,6 +0,0 @@
-def count_words(word)
-    word = word.downcase.split(" ")
-    
-    word.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
-end
-print count_words("cake cake caKe I like cake very much please gIve me cake I need the cake i need it")
